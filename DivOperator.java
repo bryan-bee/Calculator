639d605e6418a202600dfea79a2b39cd5ded6017
bryan-bee <78223974+bryan-bee@users.noreply.github.com>
2021-02-23 23:26:25 -0800

    Update DivOperator.java

--- DivOperator.java
+++ DivOperator.java
@@ -1,15 +1,3 @@
-//********************************************************************************************************
-// CLASS: DivOperator (DivOperator.java)
-//
-// COURSE AND PROJECT INFO
-// CSE205 Object Oriented Programming and Data Structures, Spring 2021
-// Project Number: 4
-// Team Name: gg
-// AUTHOR:
-// Bryan Bee, bbee2, bbee2@asu.edu
-// Camille Wolf, ceswan, ceswan@asu.edu
-// Anthony Passetti, apassett, apassett@asu.edu
-//********************************************************************************************************
 
 /**
  * Represents the division operator which is a specific type of binary operator.
