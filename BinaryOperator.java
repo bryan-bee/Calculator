066db67ecde8677e2c8df602b70ff4d6e4d8eaef
bryan-bee <78223974+bryan-bee@users.noreply.github.com>
2021-02-23 23:24:59 -0800

    Update BinaryOperator.java

--- BinaryOperator.java
+++ BinaryOperator.java
@@ -1,16 +1,3 @@
-//********************************************************************************************************
-// CLASS: BinaryOperator (BinaryOperator.java)
-//
-// COURSE AND PROJECT INFO
-// CSE205 Object Oriented Programming and Data Structures, Spring 2021
-// Project Number: 4
-// TEAM NAME: gg
-// AUTHOR:
-// Bryan Bee, bbee2, bbee2@asu.edu
-// Camille Wolf, ceswan, ceswan@asu.edu
-// Anthony Passetti, apassett, apassett@asu.edu
-//********************************************************************************************************
-
 /**
  * BinaryOperator is the superclass of all binary operators.
  */
