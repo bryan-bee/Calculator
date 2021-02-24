09e7c3511e6d2ebd069f00c731f4d2d87342d777
bryan-bee <78223974+bryan-bee@users.noreply.github.com>
2021-02-23 23:27:33 -0800

    Update Operator.java

--- Operator.java
+++ Operator.java
@@ -1,16 +1,3 @@
-//********************************************************************************************************
-// CLASS: Operator (Operator.java)
-//
-// COURSE AND PROJECT INFO
-// CSE205 Object Oriented Programming and Data Structures, Spring 2021
-// Project Number: 4
-// Team Name: gg
-// AUTHOR:
-// Bryan Bee, bbee2, bbee2@asu.edu
-// Camille Wolf, ceswan, ceswan@asu.edu
-// Anthony Passetti, apassett, apassett@asu.edu
-//********************************************************************************************************
-
 /**
  * Operator is the abstract superclass of all binary and unary operators.
  */
