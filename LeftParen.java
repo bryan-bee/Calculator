1e896d2ec88fce13dbc047b2d930166982387901
bryan-bee <78223974+bryan-bee@users.noreply.github.com>
2021-02-23 23:27:10 -0800

    Update LeftParen.java

--- LeftParen.java
+++ LeftParen.java
@@ -1,16 +1,3 @@
-//********************************************************************************************************
-// CLASS: LeftParen (LeftParen.java)
-//
-// COURSE AND PROJECT INFO
-// CSE205 Object Oriented Programming and Data Structures, Spring 2021
-// Project Number: 4
-// Team Name: gg
-// AUTHOR:
-// Bryan Bee, bbee2, bbee2@asu.edu
-// Camille Wolf, ceswan, ceswan@asu.edu
-// Anthony Passetti, apassett, apassett@asu.edu
-//********************************************************************************************************
-
 /**
  * Represents a left parenthesis in the expression.
  */
