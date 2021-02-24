f2913cefc1fd8462a32f17d83c1b350665ed9c61
bryan-bee <78223974+bryan-bee@users.noreply.github.com>
2021-02-23 23:26:57 -0800

    Update Expression.java

--- Expression.java
+++ Expression.java
@@ -1,15 +1,3 @@
-//********************************************************************************************************
-// CLASS: Expression (Expression.java)
-//
-// COURSE AND PROJECT INFO
-// CSE205 Object Oriented Programming and Data Structures, Spring 2021
-// Project Number: 4
-// Team Name: gg
-// AUTHOR:
-// Bryan Bee, bbee2, bbee2@asu.edu
-// Camille Wolf, ceswan, ceswan@asu.edu
-// Anthony Passetti, apassett, apassett@asu.edu
-//********************************************************************************************************
 
 /**
  * Represents an infix expression to be evaluated.
